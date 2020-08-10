4054999e21eb4eb30b554f4bb495c15ae92689be
Christopher Sinlasath <csinlasath@live.com>
2020-08-09 21:07:38 -0500

    Added PUBLIC URL ENV VARIABLE

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import DirContainer from "./directory/dirContainer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={DirContainer} />
+        <Route
+          exact
+          path={`${process.env.PUBLIC_URL}/`}
+          component={DirContainer}
+        />
       </div>
     </Router>
   );
